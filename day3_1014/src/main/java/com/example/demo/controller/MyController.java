dbf5ad9c328dc4ea2a18896c056ef7dbd25cc333
lsw1232414 <limsw980821@gmail.com>
2025-10-14 14:26:32 +0900

    1014

--- day3_1014/src/main/java/com/example/demo/controller/MyController.java
+++ day3_1014/src/main/java/com/example/demo/controller/MyController.java
@@ -18,11 +18,23 @@ public class MyController {
 	public MyController(MyService myService) {
 		this.myService = myService;
 	}
+	@GetMapping("/ex324")
+	public String ex324() {
+		return myService.ex324();
+	}
+	@GetMapping("/ex325")
+	public String ex325() {
+		return myService.ex325();
+	}
+	@GetMapping("/ex326")
+	public String ex326() {
+		return myService.ex326();
+	}
+	@GetMapping("/ex327")
+	public String ex327() {
+		return myService.ex327();
+	}
 	
-//	@GetMapping("/")
-//	public String getMethodName() {
-//		return myService.sayHello("안녕");
-//	}
 	@GetMapping("/ex328")
 	public String ex328() {
 		return myService.ex328();
--- day3_1014/src/main/java/com/example/demo/service/MyService.java
+++ day3_1014/src/main/java/com/example/demo/service/MyService.java
@@ -13,6 +13,75 @@ public class MyService {
 
     private static final Logger log = LoggerFactory.getLogger(MyService.class);
     
+    public interface Car {
+        String getColor();
+    }
+
+    public class Sonata implements Car {
+        public Sonata() {
+            log.info("=== 출고 === Sonata");
+        }
+        @Override
+        public String getColor() {
+            return "=== 색상: RED === Sonata";
+        }
+    }
+
+    public class K5 implements Car {
+        public K5() {
+            log.info("=== 출고 === K5");
+        }
+        @Override
+        public String getColor() {
+            return "=== 색상: YELLOW === K5";
+        }
+    }
+    
+    public String ex324() {
+        Car car1 = new Sonata();
+        String re1 = car1.getColor();
+
+        Car car2 = new K5();
+        String re2 = car2.getColor();
+
+        String result = re1 + "<br>" + re2 + "<br>" + "차 두 대를 출고하였습니다.";
+        log.info("ex324() 실행 완료");
+        return result;
+    }
+    public String ex325() {
+    	List list = new ArrayList<Integer>();
+    	
+    	list.add(1);
+    	list.add(2);
+    	list.add(3);
+    	
+    	System.out.println(list.get(1));
+    	return "콘솔 출력";
+    }
+    public String ex326() {
+    	List list = new ArrayList<String>();
+    	
+    	list.add("public");       
+        list.add("static");     
+        list.add("void");        
+
+        
+        for (int i = 0; i < list.size(); i++) { 
+            System.out.println(list.get(i));   
+        }
+
+        list.remove(1);    
+        int voidIndex = list.indexOf("void");  
+        System.out.println("void의 index = " + voidIndex);
+        return "콘솔 출력";
+    }
+    public String ex327() {
+    	String str1 = new String("is same?");
+    	String str2 = new String("is same?");
+    	
+    	System.out.println(str1 == str2);
+    	return "콘솔 출력";
+    }
     public String ex328() {
         String str1 = new String("is same?");
         String str2 = new String("is same?");
