075dc66b4784665c5a272b0b191ed489f10e1a6b
lsw1232414 <limsw980821@gmail.com>
2025-10-14 11:30:00 +0900

    1014

---
+++ day01_1010/demo3-1/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		6896753323118
+	}
+}
+
---
+++ day3_1014/src/main/java/com/example/demo/Day31014Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day31014Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day31014Application.class, args);
+	}
+
+}
