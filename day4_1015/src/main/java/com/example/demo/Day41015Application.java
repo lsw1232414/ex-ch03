695b9025f17cb0a1942a5a2e2146f3b3fb98521f
lsw1232414 <limsw980821@gmail.com>
2025-10-15 09:13:00 +0900

    1015

---
+++ day4_1015/src/main/java/com/example/demo/Day41015Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day41015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day41015Application.class, args);
+	}
+
+}
