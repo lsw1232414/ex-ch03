c1c7fed9a66e986e668d81c1f2f13b9f4d2d9ea6
lsw1232414 <limsw980821@gmail.com>
2025-10-17 12:24:52 +0900

    1017

---
+++ day6_1017/src/main/java/com/lsw/myform/Day61017Application.java
@@ -0,0 +1,13 @@
+package com.lsw.myform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day61017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day61017Application.class, args);
+	}
+
+}
---
+++ day6_1017/src/test/java/com/example/demo/Day61017ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day61017ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
